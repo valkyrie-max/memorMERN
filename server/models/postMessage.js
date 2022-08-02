a633f0bdfb0e161cd3d0c8ebccd59701a6ce2217
valkyrie-max <alisakitkina2016@gmail.com>
2022-08-02 00:42:20 -0500

    createdd post message model with mongoose

---
+++ server/models/postMessage.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'; 
+
+// mongoose allows to give uniformity to documents
+   // specifying what each posts has to have 
+const postSchema = mongoose.Schema({   
+   title: String,
+   message: String,
+   creator: String,
+   tags: [String],
+   // will convert image to a string
+   selectedFiler: String,
+   likeCount: {
+      type: Number,
+      default: 0
+   },
+   createdAt: {
+      type: Date,
+      default: new Date()
+   }
+});
+
+const PostMessage = mongoose.model('PostMessage', postSchema)
+
+// exporting mongoose model
+export default PostMessage;
