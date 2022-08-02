ea547e8498640ad6b5e4c661f55486c3d3ea0bf0
valkyrie-max <alisakitkina2016@gmail.com>
2022-08-02 00:36:02 -0500

    added a simple controller

---
+++ server/controllers/posts.js
@@ -0,0 +1,6 @@
+// all the handlers for the routes 
+   // extracting all the logic from the routes
+
+export const getPosts = (req, res) => {
+   res.send('THIS WORKS!'); 
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,13 @@
 import express from 'express';
 
+import { getPosts } from '../controllers/posts.js';
+
 const router = express.Router();
 
 // 1) callback executed is on localhost:5000/ 
    // req = request
    // res = response
       // reached through http://localhost:5000/posts
-router.get('/', (req, res) => {
-   res.send('THIS WORKS!'); 
-})
+router.get('/', getPosts)
 
 export default router; 
